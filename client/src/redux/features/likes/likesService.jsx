9b04489340c24ef342b87fee25dd0d616805116a
elsaaeid <saidsadaoy@gmail.com>
2024-06-08 10:39:21 -0700

    deploy changes

--- client/src/redux/features/likes/likesService.jsx
+++ client/src/redux/features/likes/likesService.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-import {BACKEND_URL} from "../../helper";
+// import {BACKEND_URL} from "../../helper";
 
 
-const API_URL = `${BACKEND_URL}/api/likes/increment/`;
+const API_URL = `${process.env.BACKEND_URL}/api/likes/increment/`;
 
 // Increase New Likes
 const incrementLikes = async ({ postId, userId }) => {
